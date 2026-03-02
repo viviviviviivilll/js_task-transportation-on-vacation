e6123cc0ea8f0af7b8fa95b59add84825cbc992c
viviviviviivilll <bbojcuk44@gmail.com>
2026-03-02 18:02:59 +0200

    #1 version

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,26 @@
+const LITTLE_PRICE_OFF = 20;
+const BIG_PRICE_OFF = 50;
+const PRICE_PER_ONE_DAY = 40;
+const DAYS_FOR_LITTLE__PRICE_OFF = 3;
+const DAYS_FOR_BIG__PRICE_OFF = 7;
+
 /**
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const finalPrice = days * PRICE_PER_ONE_DAY;
+
+  if (days < DAYS_FOR_LITTLE__PRICE_OFF) {
+    return finalPrice;
+  }
+
+  if (days >= DAYS_FOR_LITTLE__PRICE_OFF && days < DAYS_FOR_BIG__PRICE_OFF) {
+    return finalPrice - LITTLE_PRICE_OFF;
+  }
+
+  return finalPrice - BIG_PRICE_OFF;
 }
 
 module.exports = calculateRentalCost;
